babed1d2859b86fb3dc061a0982f5a5607321fcb
Roopika21 <96283014+Roopika21@users.noreply.github.com>
2025-03-27 11:24:11 +0530

    Update and rename add.java to add.py

--- add.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner; // Import the Scanner class
-class add {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 6;
-    int sum = x + y;
-    System.out.println(sum);
-  }
-} 
